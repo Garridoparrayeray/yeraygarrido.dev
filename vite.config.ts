256ac58ff34c792b00fe9fde8996317f5ea40d85
Garridoparrayeray <garridoparrayeray@gmail.com>
2026-02-26 22:51:20 +0100

    posibilidad de implementar api key gemini quitado, data leak. Para implementar ia con n8n usare Serverless Functions.

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,45 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+  
   return {
     plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
+    build: {
+      cssCodeSplit: true, 
+      // Limpieza de la consola en producción para ahorrar ejecución de JS
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
+      rollupOptions: {
+        output: {
+          // SEPARACIÓN DE LIBRERÍAS PESADAS 
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              // Separamos GSAP : pesado y se usa poco
+              if (id.includes('gsap')) return 'vendor-gsap';
+              // el calendario de GitHub que tarda mas en procesarse se separa de los demas componentes
+              if (id.includes('react-github-calendar')) return 'vendor-github';
+              // El resto de librerías base (React, etc)
+              return 'vendor';
+            }
+          },
+        },
+      },
+    },
     server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
